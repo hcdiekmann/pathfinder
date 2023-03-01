d7dc32ee6f7dd546a1ea4e84e81a6554bb2c6710
hcdiekmann <hcdiekmann@hotmail.com>
2023-03-01 12:37:18 +0100

    rename single robot sim launch

---
+++ launch/single_bot_sim.launch.py
@@ -0,0 +1,41 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    package_name='pathfinder'
+
+     # Include the robot_state_publisher launch file and force sim time to be enabled
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','robot_state_pub.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'pathfinder'],
+                        output='screen')
+
+
+    # Launch everything!
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity,
+    ])
