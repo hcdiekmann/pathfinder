89b645836de58c20d728cbcebd92507b5167f911
hcdiekmann <hcdiekmann@hotmail.com>
2023-04-02 22:29:56 +0200

    adds depth camera

--- launch/single_bot_sim.launch.py
+++ launch/single_bot_sim.launch.py
@@ -4,23 +4,31 @@ import xacro
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, RegisterEventHandler, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.event_handlers import OnExecutionComplete
 from launch_ros.actions import Node
+from launch.actions import (IncludeLaunchDescription, DeclareLaunchArgument, EmitEvent, ExecuteProcess,
+                            LogInfo, RegisterEventHandler, TimerAction)
+from launch.conditions import IfCondition
+from launch.event_handlers import (OnExecutionComplete, OnProcessExit,
+                                OnProcessIO, OnProcessStart, OnShutdown)
+from launch.events import Shutdown
+from launch.substitutions import (EnvironmentVariable, FindExecutable,
+                                LaunchConfiguration, LocalSubstitution,
+                                PythonExpression)
 
 
 def generate_launch_description():
 
     pkg_pathfinder = get_package_share_directory('pathfinder')
     nav2_bringup = os.path.join(get_package_share_directory('nav2_bringup'))
+    slam_toolbox = os.path.join(get_package_share_directory('slam_toolbox'))
     
     # Process the URDF xacro file
     xacro_file = os.path.join(pkg_pathfinder,'description','robot.urdf.xacro')
     urdf = xacro.process_file(xacro_file)
     
     # Include the robot_state_publisher to publish transforms
-    rsp = Node(
+    rsp_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
@@ -28,7 +36,7 @@ def generate_launch_description():
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
-    world = os.path.join(pkg_pathfinder, 'worlds', 'maze.world') 
+    world = os.path.join(pkg_pathfinder, 'worlds', 'yolo.world') 
      # Gazebo launch
     start_gazebo = ExecuteProcess(
         cmd=[
@@ -44,7 +52,7 @@ def generate_launch_description():
     )
 
     # Run the spawner node from the gazebo_ros package to spawn the robot in the simulation
-    spawn_entity = Node(
+    spawn_entity_node = Node(
                 package='gazebo_ros', 
                 executable='spawn_entity.py',
                 output='screen',
@@ -55,7 +63,7 @@ def generate_launch_description():
     # Load SLAM parameters from yaml file and add node to launch description
     slam_params_file = os.path.join(pkg_pathfinder,'params', 'mapper_params_online_async.yaml')
     slam = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('slam_toolbox'),'launch','online_async_launch.py')]), 
+                PythonLaunchDescriptionSource([os.path.join(slam_toolbox,'launch','online_async_launch.py')]), 
                 launch_arguments={'slam_params_file': slam_params_file}.items()
     )
 
@@ -66,14 +74,35 @@ def generate_launch_description():
                 launch_arguments={'params_file': nav2_params_file}.items()
     )
 
+    # Delay the launch of nav2 by 5 seconds to give the SLAM node time to initialize and publish the map
+    delayed_nav2_launch = TimerAction(
+                period=8.0,
+                actions=[nav2])
+
     # Load explore_lite parameters from yaml file and add node to launch description
     explore_params_file = os.path.join(pkg_pathfinder, 'params', 'explore_params.yaml')
-    explore = Node(
-            package='explore_lite',
-            executable='explore',
-            output='screen',
-            parameters=[explore_params_file]
+    frontier_explore_node = Node(
+                package='explore_lite',
+                executable='explore',
+                output='screen',
+                parameters=[explore_params_file]
     )
+
+    # Launch RViz to vizualize the robot and the map in real time
+    rviz_config_file = os.path.join(pkg_pathfinder, 'rviz', 'view_depth_yolo.rviz')
+    rviz2_node = Node(
+                package='rviz2',
+                executable='rviz2',
+                output="screen",
+                arguments=[
+                    '-d', rviz_config_file,
+                ],
+    )
+    
+    delayed_rviz2_launch = TimerAction(
+                period=10.0,
+                actions=[rviz2_node])
+
     
 
     # Launch everything!
@@ -83,15 +112,30 @@ def generate_launch_description():
           default_value='true',
           description='Use simulation/Gazebo clock'
         ),
-        rsp,
+        rsp_node,
         start_gazebo,
         RegisterEventHandler(
-                    event_handler=OnExecutionComplete(
-                        target_action=spawn_entity,
-                        on_completion=[nav2] # launch nav2 after robot is spawned
-                    )
+            OnProcessStart(
+                target_action=start_gazebo,
+                on_start=[
+                    LogInfo(msg='Starting Gazebo & spawning robot...'),
+                    TimerAction(
+                        period=6.0,
+                        actions=[spawn_entity_node],
+                    ),
+                ]
+            )
         ),
-        spawn_entity,
-        slam,
-        explore
+        # RegisterEventHandler(
+        #     OnProcessExit(
+        #         target_action=spawn_entity_node,
+        #         on_exit=[
+        #             LogInfo(msg='Robot spawned, starting SLAM...'),
+        #             slam
+        #         ]
+        #     )
+        # ),
+        # delayed_nav2_launch,
+        delayed_rviz2_launch,
+        # frontier_explore_node # Uncomment this line to enable the explore_lite node to start mapping the environment
     ])
